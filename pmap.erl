d55af34fc145cc56514047735b67a0bcbfeba274
Yutaro Tsunekawa <tsunekawa@ueda.info.waseda.ac.jp>
2019-08-16 11:13:58 +0900

    update pmap

--- pmap.erl
+++ pmap.erl
@@ -6,18 +6,35 @@ fact(0) ->
 fact(X) ->
     X * fact(X-1).
 
-pmap(F, Ls) ->
+pmap(F, L) ->
     S = self(),
-    L = lists:map(fun(X) -> spawn(fun() -> S ! {self(), F(X)} end) end, Ls),
-    lists:map(fun(Pid) -> 
-		      receive 
-			  {Pid, Res} -> Res
-		      end
-	      end, L).
+    Ref = erlang:make_ref(),
+    Pids = lists:map(fun(I) ->
+			     spawn(fun() -> do_f(S, Ref, F, I) end)
+		     end, L),
+    gather(Pids, Ref).
+
+do_f(Parent, Ref, F, I) ->
+    Parent ! {self(), Ref, (catch F(I))}.
+
+gather([Pid|T], Ref) ->
+    receive
+	{Pid, Ref, Ret} ->
+	    [Ret|gather(T, Ref)]
+    end;
+gather([], _) ->
+    [].
+
+    %% L = lists:map(fun(X) -> spawn(fun() -> S ! {self(), F(X)} end) end, Ls),
+    %% lists:map(fun(Pid) -> 
+    %% 		      receive 
+    %% 			  {Pid, Res} -> Res
+    %% 		      end
+    %% 	      end, L).
     
 run(N) ->
     X = floor(math:pow(10, N)),
     L = lists:map(fun(_) -> floor(rand:uniform() * X) end, lists:seq(1, X)),
-    pmap(fact, L).
+    pmap(fun fact/1, L).
     %% timer:tc(pmap, pmap, [fact, L]).
     %% io:format("~w~n", [timer:tc(pmap, pmap, [fact, L])]).
